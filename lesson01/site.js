871cc85b29d8f79346761f1dbc4bef17d626c52e
Jose Luiz da Silva Borges <A01196135@ad.bcit.ca>
2020-01-07 18:30:03 -0800

    Initial Commit

---
+++ lesson01/site.js
@@ -0,0 +1,15 @@
+ // document.getElementById("heading").style.color = "red"
+
+ document.querySelector("#heading").style.color = 'green';
+ 
+
+ // js ``
+ // ES6 string/template
+
+ 
+var year = "1000.4";
+ 
+
+let t = typeof(year) === "string" ? 'Is string' : 'Not string'
+
+console.log(t)
