4a5a14915019c75907fe497cec2fdb20c9533dd7
shpsyte <jose.iscosistemas@gmail.com>
2020-02-15 13:53:53 -0800

    Assigment 05

--- day-05/a-05-start/scripts/menu.js
+++ day-05/a-05-start/scripts/menu.js
@@ -1,36 +1,21 @@
-// JavaScript Document
-
-// Menu JS
-const body = document.body;
-const hamburger = document.querySelector('.hamburger-menu');
-const bar = document.querySelector('.bar');
-
-hamburger.addEventListener('click', showMenu);
-
-function showMenu(){
-    body.classList.toggle('show');
-    bar.classList.toggle('animate');
-}
-
 // Responsive Menu - Dropdown
 const body = document.body;
-const menu = document.querySelector('.hamburger-menu');
-const bar = document.querySelector('.bar');
-const nav = document.getElementById('main-navigation');
+const menu = document.querySelector(".hamburger-menu");
+const bar = document.querySelector(".bar");
+const nav = document.getElementById("main-navigation");
 
-// Below code for preventing nav from animating on 
+// Below code for preventing nav from animating on
 // browser re-size modified from code found at
 // this stackoverflow question and answer:
 //
 // https://goo.gl/6s3pAZ
 
+menu.addEventListener("click", openMenu);
 
-menu.addEventListener('click', openMenu);
-
-function openMenu(){
-    body.classList.toggle('show');
-    nav.classList.add('activated');
-    bar.classList.toggle('animate');
+function openMenu() {
+  body.classList.toggle("show");
+  nav.classList.toggle("activated");
+  bar.classList.toggle("animate");
 }
 
 // Media Query Event Listener
@@ -40,9 +25,9 @@ function openMenu(){
 
 // Create a media query list using
 // matchMedia
-const mql = window.matchMedia('(min-width: 560px)');
+const mql = window.matchMedia("(min-width: 560px)");
 
-// Add a Media Query Listener to the 
+// Add a Media Query Listener to the
 // above media query list
 mql.addListener(removeTransition);
 
@@ -52,18 +37,18 @@ mql.addListener(removeTransition);
 // sizes. In this case when the
 // screen width becomes wider then
 // 560px
-function removeTransition(e){
-	// e -> is the event object
-	// e.matches -> stores a true false
-	// value depending if the media query
-	// list set above matches or not
-	if(e.matches){
-		body.classList.remove('show');
-		nav.classList.remove('activated');
-	}
+function removeTransition(e) {
+  // e -> is the event object
+  // e.matches -> stores a true false
+  // value depending if the media query
+  // list set above matches or not
+  if (e.matches) {
+    body.classList.remove("show");
+    nav.classList.remove("activated");
+  }
 }
 
 // Prevents the focus state from activating
-menu.addEventListener('mousedown', function(e){
-    e.preventDefault();
+menu.addEventListener("mousedown", function(e) {
+  e.preventDefault();
 });
